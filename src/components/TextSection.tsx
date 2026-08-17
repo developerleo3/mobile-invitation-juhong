c61f483a13f776ad1a24783be8b621942731333d
taeke2 <taeke2@naver.com>
2026-08-17 17:26:18 +0900

    refactor(Text): 결혼 문구 수정

--- src/components/TextSection.tsx
+++ src/components/TextSection.tsx
@@ -14,28 +14,18 @@ export default function TextSection() {
                 </FadeInOnView>
 
                 {/* 본문(시/문구) */}
-                <div className="mt-10 text-[12px] font-nanum-myeongjo leading-relaxed">
+                <div className="mt-20 text-[12px] font-nanum-myeongjo leading-relaxed">
                     <FadeInOnView>
                         <p>
-                            사랑은 소유가 아니라<br/>
-                            동행임을 아는 두 사람은<br/><br/>
-
-                            잡은 손을 놓지 않되<br/>
-                            함부로 잡아끌지 않을 것이며<br/><br/>
+                            찬란했던 20대를 나란히 걸으며<br/>
+                            서로의 가장 예쁜 시절을 지켜보았습니다<br/><br/>
                         </p>
                     </FadeInOnView>
                     <FadeInOnView>
                         <p>
-                            서로의 두 눈을 고요히 바라보아<br/>
-                            말하지 않아도 같은 쪽으로 걸어가리라<br/><br/>
-
-                            - 박미라, ‘아름다운 날에 부치다’ 중에서 -<br/><br/>
-                        </p>
-                    </FadeInOnView>
-                    <FadeInOnView>
-                        <p className="mt-3 font-bold">
-                            서로의 곁을 선택한 두 사람이 이제 함께합니다.<br/>
-                            그 시작을 함께해 주시면 감사하겠습니다.
+                            이제 그 오랜 믿음과 사랑으로<br/>
+                            평생을 약속하고자 합니다<br/>
+                            축복의 걸음으로 자리를 빛내주시면 감사하겠습니다<br/><br/>
                         </p>
                     </FadeInOnView>
                 </div>
@@ -43,15 +33,15 @@ export default function TextSection() {
 
                 {/* 하단 이름 */}
                 <FadeInOnView>
-                    <div className="mt-10 text-center font-nanum-myeongjo text-[12px] font-bold">
+                    <div className="mt-20 text-center font-nanum-myeongjo text-[12px] font-bold">
                         <div className="inline-grid grid-cols-[auto_auto_auto] gap-x-3 gap-y-0.5">
-                            <span className="text-right">허정행 · 박성연의</span>
+                            <span className="text-right">이연수 · 김정화의</span>
                             <span>아들</span>
-                            <span>허성택</span>
+                            <span>이주홍</span>
 
-                            <span className="text-right">이형석 · 류정란의</span>
+                            <span className="text-right">최종구 · 박준하의</span>
                             <span>딸</span>
-                            <span>이현정</span>
+                            <span>최미향</span>
                         </div>
                     </div>
                 </FadeInOnView>
